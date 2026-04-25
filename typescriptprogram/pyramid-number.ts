fc337e165b88504763ae2b43bd307f7a62bc11b9
Prathamesh <Prathamesh.moreqa@gmail.com>
2026-04-25 23:46:38 +0530

    typescript pyrmid program added

--- typescriptprogram/pyramid-number.ts
+++ typescriptprogram/pyramid-number.ts
@@ -3,13 +3,13 @@
 function printNumberPyramid(rows: number): void {
     for (let i = 1; i <= rows; i++) {
         // Print spaces
-        let spaces = ' '.repeat(rows - i);
+        //let spaces = ' '.repeat(rows - i);
         // Print numbers
         let numbers = '';
         for (let j = 1; j <= i; j++) {
             numbers += j + ' ';
         }
-        console.log(spaces + numbers);
+        console.log(numbers);
     }
 }
 
